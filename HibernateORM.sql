08de406f3f80e17ccb4956140805bfa5cd5fda05
Димитър Янакиев <dyanakiev@codingburgas.bg>
2023-02-14 00:40:24 +0200

    Add database querry

---
+++ HibernateORM.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE HibernateORM
+
+USE HibernateORM
+
+CREATE TABLE book (
+	id INT,
+	book_name VARCHAR(50),
+	author_id INT
+);
+INSERT INTO book (id, book_name, author_id) VALUES (1, 'Veribet', 1);
+INSERT INTO book (id, book_name, author_id) VALUES (2, 'Zathin', 2);
+INSERT INTO book (id, book_name, author_id) VALUES (3, 'Kanlam', 3);
+INSERT INTO book (id, book_name, author_id) VALUES (4, 'Subin', 4);
+INSERT INTO book (id, book_name, author_id) VALUES (5, 'Treeflex', 5);
+INSERT INTO book (id, book_name, author_id) VALUES (6, 'Cookley', 6);
+INSERT INTO book (id, book_name, author_id) VALUES (7, 'Duobam', 7);
+INSERT INTO book (id, book_name, author_id) VALUES (8, 'Cardguard', 8);
+INSERT INTO book (id, book_name, author_id) VALUES (9, 'Kanlam', 9);
+INSERT INTO book (id, book_name, author_id) VALUES (10, 'Tresom', 10);
+INSERT INTO book (id, book_name, author_id) VALUES (11, 'Fix San', 11);
+INSERT INTO book (id, book_name, author_id) VALUES (12, 'Treeflex', 12);
+INSERT INTO book (id, book_name, author_id) VALUES (13, 'Zamit', 13);
+INSERT INTO book (id, book_name, author_id) VALUES (14, 'Bigtax', 14);
+INSERT INTO book (id, book_name, author_id) VALUES (15, 'It', 15);
+INSERT INTO book (id, book_name, author_id) VALUES (16, 'Wrapsafe', 16);
+INSERT INTO book (id, book_name, author_id) VALUES (17, 'Y-Solowarm', 17);
+INSERT INTO book (id, book_name, author_id) VALUES (18, 'Bitwolf', 18);
+INSERT INTO book (id, book_name, author_id) VALUES (19, 'Flowdesk', 19);
+INSERT INTO book (id, book_name, author_id) VALUES (20, 'Sonsing', 20);
+INSERT INTO book (id, book_name, author_id) VALUES (21, 'Ventosanzap', 21);
+INSERT INTO book (id, book_name, author_id) VALUES (22, 'It', 22);
+INSERT INTO book (id, book_name, author_id) VALUES (23, 'Zathin', 23);
+INSERT INTO book (id, book_name, author_id) VALUES (24, 'Subin', 24);
+INSERT INTO book (id, book_name, author_id) VALUES (25, 'Stim', 25);
+INSERT INTO book (id, book_name, author_id) VALUES (26, 'Home Ing', 26);
+INSERT INTO book (id, book_name, author_id) VALUES (27, 'Ronstring', 27);
+INSERT INTO book (id, book_name, author_id) VALUES (28, 'Voltsillam', 28);
+INSERT INTO book (id, book_name, author_id) VALUES (29, 'Konklab', 29);
+INSERT INTO book (id, book_name, author_id) VALUES (30, 'Prodder', 30);
